5b5160110d59da762d905cb6fd105e57f4fa9487
Arthur Deruytter <art.deruytter@gmail.com>
2020-03-03 02:45:27 +0100

    Edit server start for windows
    Made .env.bat.template file and changed runapp.bat file to incorporate .env.bat file

--- runapp.bat
+++ runapp.bat
@@ -1,7 +1,4 @@
-set GEOCODE_DATABASE_URL=jdbc:mariadb://localhost:3306
-set GEOCODE_DATABASE_USERNAME=geocode
-set GEOCODE_DATABASE_PASSWORD=geocode
-set GEOCODE_PORT=8080
-set JAVA_HOME="C:\Program Files\Java\jdk-11.0.1"
+
+call .env.bat
 
 gradlew.bat %*
