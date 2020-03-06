3de8afe4dbd5178c84f4f33b731735b17787fa3b
Francis <francisklinck@gmail.com>
2020-03-06 15:19:20 +0100

    add small test to see how things work

--- src/test/kotlin/be/ugent/webdevelopment/backend/geocode/database/RepositoryTest.kt
+++ src/test/kotlin/be/ugent/webdevelopment/backend/geocode/database/RepositoryTest.kt
@@ -1,21 +1,16 @@
 package be.ugent.webdevelopment.backend.geocode.database
 
+import be.ugent.webdevelopment.backend.geocode.database.models.Location
 import be.ugent.webdevelopment.backend.geocode.database.models.OauthParty
 import be.ugent.webdevelopment.backend.geocode.database.models.User
 import be.ugent.webdevelopment.backend.geocode.database.repositories.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.platform.commons.logging.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.logging.LogLevel
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.data.jpa.repository.Query
-import java.util.logging.LogManager
-import java.util.logging.Logger
-
+import java.util.*
 
 @DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) //https://stackoverflow.com/questions/41315386/spring-boot-1-4-datajpatest-error-creating-bean-with-name-datasource
 class RepositoryTest @Autowired constructor (
         val userTourRepository: UserTourRepository,
         val userRepository: UserRepository,
@@ -27,12 +22,33 @@ class RepositoryTest @Autowired constructor (
         val commentRepository: CommentRepository,
         val checkInRepository: CheckInRepository) {
 
-    val oauthParty = OauthParty(oauthPartyName = "Google")
-    val user = User(oauthId = "1234", oauthParty = oauthParty, username = "Jhon", email = "john@mail.com")
+    private val oauthParty = OauthParty(oauthPartyName = "Google")
+    private val user = User(oauthId = "1234", oauthParty = oauthParty, username = "Jhon", email = "john@mail.com")
+    private val location = Location(longitude = 2.0, latitude = 4.0, listed = false, name = "Epic Location",
+            description = "...", creator = user)
 
     @Test
     fun oauthPartyRepositoryTest() {
         oauthPartyRepository.save(oauthParty)
-        oauthPartyRepository.findAll().forEach{ it -> println("${it.id} - ${it.oauthPartyName}") }
+        val found = oauthPartyRepository.findByOauthPartyName(oauthParty.oauthPartyName)
+        assertThat(found).isNotNull
+        assertThat(found).isEqualTo(oauthParty)
+    }
+
+    @Test
+    fun userRepositoryTest() {
+        userRepository.save(user)
+        val found: Optional<User> = userRepository.findById(user.id)
+        assertThat(found).isNotNull
+        assertThat(userRepository.findByUsername("fred").size).isEqualTo(0)
+        assertThat(userRepository.findByUsername(user.username).size).isEqualTo(1)
+        assertThat(found.get().oauthParty).isEqualTo(oauthParty)
+    }
+
+    @Test
+    fun locationRepositoryTest() {
+        locationRepository.save(location)
+        val found = locationRepository.findByCreator(user)
+        assertThat(found.size).isEqualTo(1)
     }
 }
