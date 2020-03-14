6c660b2fd36ade9bdf2e518f74959a5a2d16e86d
Francis <francisklinck@gmail.com>
2020-03-15 00:52:00 +0100

    remove whitepages

--- src/main/kotlin/be/ugent/webdevelopment/backend/geocode/GeocodeApplication.kt
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/GeocodeApplication.kt
@@ -1,9 +1,10 @@
 package be.ugent.webdevelopment.backend.geocode
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = arrayOf(ErrorMvcAutoConfiguration::class))
 class GeocodeApplication
 
 fun main(args: Array<String>) {
