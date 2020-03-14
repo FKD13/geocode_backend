3540a76483c3e5732b11dae9e7d2058eb2cb0b1e
Francis <francisklinck@gmail.com>
2020-03-14 22:33:36 +0100

    add chaptaVerification

---
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/services/CaptchaService.kt
@@ -0,0 +1,54 @@
+package be.ugent.webdevelopment.backend.geocode.services
+
+import be.ugent.webdevelopment.backend.geocode.exceptions.GenericException
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.postForObject
+import java.net.URI
+import java.util.regex.Pattern
+
+class CaptchaResponse(
+    val success: Boolean?,
+
+    @JsonProperty("challenge_ts")
+    val challengeTs: String?,
+    val action: String?,
+    val hostname: String?,
+
+    @JsonProperty("error-codes")
+    val errorCodes: List<Int>?
+)
+
+@Service
+class CaptchaService {
+
+    @Value("\${CHAPTCHA_SITE}")
+    private final lateinit var captchaSite: String
+
+    @Value("\${CHAPTCHA_SECRET}")
+    private final lateinit var captchaSecret: String
+
+    private final val action: String = "register"
+
+    private val captchaPattern = Pattern.compile("[A-Za-z0-9_-]+")
+
+    fun checkRespone(response: String) : Boolean {
+        return response.isNotEmpty() && captchaPattern.matcher(response).matches()
+    }
+
+    fun validateCaptcha(response: String) {
+        if (!checkRespone(response)) {
+            throw GenericException(code = HttpStatus.BAD_REQUEST, message = "Not a valid captha response")
+        }
+        val verifyUri: URI = URI.create(
+                "https://www.google.com/recaptcha/api/siteverify?secret=${captchaSecret}&response=${response}")
+        val captchaResponse : CaptchaResponse = RestTemplate().postForObject(verifyUri, CaptchaResponse::class)
+        if (captchaResponse.success == null || !captchaResponse.success ) {
+            throw GenericException(code = HttpStatus.BAD_REQUEST, message = "Captha failed")
+        } else if (captchaResponse.action == null || captchaResponse.action == action) {}
+    }
+
+}
