1e2a667bf81e2114df3a57fb7d05307ad75fa1ac
Arthur Deruytter <arthur.deruytter@ugent.be>
2020-03-15 19:09:22 +0100

    fix user and users opsplitsing

--- src/main/kotlin/be/ugent/webdevelopment/backend/geocode/controllers/UserController.kt
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/controllers/UserController.kt
@@ -4,7 +4,6 @@ import be.ugent.webdevelopment.backend.geocode.controllers.wrappers.LocationWrap
 import be.ugent.webdevelopment.backend.geocode.controllers.wrappers.UserWrapper
 import be.ugent.webdevelopment.backend.geocode.services.JWTAuthenticator
 import be.ugent.webdevelopment.backend.geocode.services.LocationsService
-import be.ugent.webdevelopment.backend.geocode.services.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
@@ -14,7 +13,7 @@ import javax.servlet.http.HttpServletResponse
 @RestController
 @ResponseStatus(HttpStatus.OK)
 @RequestMapping("/user")
-class UserController(val service: UserService, val jwtService: JWTAuthenticator, val locationsService: LocationsService) : Controller<UserWrapper>{
+class UserController(val jwtService: JWTAuthenticator, val locationsService: LocationsService) : Controller<UserWrapper>{
 
     @GetMapping
     fun findByLoggedIn(
@@ -27,16 +26,5 @@ class UserController(val service: UserService, val jwtService: JWTAuthenticator,
         return locationsService.findAllByUser(jwtService.tryAuthenticate(request))
     }
 
-    @PatchMapping
-    fun update(@RequestBody resource: UserWrapper,
-               response: HttpServletResponse, request: HttpServletRequest) {
-        service.update(jwtService.tryAuthenticate(request), resource)
-    }
-
-    @DeleteMapping
-    fun delete(
-               response: HttpServletResponse, request: HttpServletRequest) {
-        service.deleteUser(jwtService.tryAuthenticate(request))
-    }
 
 }
--- src/main/kotlin/be/ugent/webdevelopment/backend/geocode/services/UsersService.kt
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/services/UsersService.kt
@@ -1,11 +1,16 @@
 package be.ugent.webdevelopment.backend.geocode.services
 
+import be.ugent.webdevelopment.backend.geocode.controllers.wrappers.UserWrapper
 import be.ugent.webdevelopment.backend.geocode.controllers.wrappers.UsersWrapper
 import be.ugent.webdevelopment.backend.geocode.database.models.User
 import be.ugent.webdevelopment.backend.geocode.database.repositories.UserRepository
+import be.ugent.webdevelopment.backend.geocode.exceptions.ExceptionContainer
 import be.ugent.webdevelopment.backend.geocode.exceptions.GenericException
+import be.ugent.webdevelopment.backend.geocode.exceptions.PropertyException
 import org.springframework.stereotype.Service
 import org.springframework.beans.factory.annotation.Autowired
+import java.net.MalformedURLException
+import java.net.URL
 import java.util.*
 
 @Service
@@ -13,6 +18,9 @@ class UsersService {
 
     @Autowired
     private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var authService: AuthService
 
     fun findAll(): List<UsersWrapper> {
         return userRepository.findAll().map { user -> UsersWrapper(user) }
@@ -23,5 +31,57 @@ class UsersService {
         if(user.isEmpty) throw GenericException("User with id = $id was not found in the database")
         return UsersWrapper(user.get())
     }
+
+
+    fun checkUrl(url: String, container: ExceptionContainer){
+        try {
+            val test : URL = URL(url)
+        }catch (e : MalformedURLException){
+            container.addException(PropertyException("url", "The url is not valid."))
+        }
+    }
+
+    fun checkUsername(name: String, container: ExceptionContainer){
+        if(name.length < 3) {
+            container.addException(PropertyException("username", "Should be longer than 3 characters"))
+        }
+        if(name.length > 30) {
+            container.addException(PropertyException("username", "Should be shorter than 30 characters"))
+        }
+        if (userRepository.findByUsernameIgnoreCase(name).isEmpty.not()){
+            container.addException(PropertyException("username", "This username is already in use."))
+        }
+    }
+
+    fun update(user: User, resource: UserWrapper){
+        val container : ExceptionContainer = ExceptionContainer()
+
+        resource.email.ifPresent {
+            if(authService.checkEmail(resource.email.get()).not())
+                container.addException(PropertyException("email", "The email is not valid"))
+            checkEmailUnique(resource.email.get(), container)
+        }
+        resource.avatarUrl.ifPresent { checkUrl(resource.avatarUrl.get(), container) }
+        resource.username.ifPresent { checkUsername(resource.username.get(), container) }
+
+        container.throwIfNotEmpty()
+
+        resource.email.ifPresent { user.email = resource.email.get() }
+        resource.avatarUrl.ifPresent { user.avatarUrl = resource.avatarUrl.get() }
+        resource.username.ifPresent { user.username = resource.username.get() }
+        userRepository.saveAndFlush(user)
+
+    }
+
+    private fun checkEmailUnique(email: String, container: ExceptionContainer) {
+        userRepository.findByEmail(email).ifPresent { container.addException(PropertyException("email", "Email already exists, try again." )) }
+    }
+
+    fun deleteUser(user: User) {
+        userRepository.delete(user)
+        userRepository.flush()
+    }
+
+
 
 }
