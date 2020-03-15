8b44eabe92d142bb9f1433dca659bd9e95e97ca1
Arthur Deruytter <arthur.deruytter@ugent.be>
2020-03-15 18:32:53 +0100

    fix deletion of location endpoint

--- src/main/kotlin/be/ugent/webdevelopment/backend/geocode/controllers/LocationsController.kt
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/controllers/LocationsController.kt
@@ -1,6 +1,9 @@
 package be.ugent.webdevelopment.backend.geocode.controllers
 
+import be.ugent.webdevelopment.backend.geocode.controllers.wrappers.LocationWrapper
 import be.ugent.webdevelopment.backend.geocode.controllers.wrappers.LocationsWrapper
+import be.ugent.webdevelopment.backend.geocode.exceptions.GenericException
+import be.ugent.webdevelopment.backend.geocode.services.JWTAuthenticator
 import be.ugent.webdevelopment.backend.geocode.services.LocationsService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -11,7 +14,7 @@ import javax.servlet.http.HttpServletResponse
 @RestController
 @ResponseStatus(HttpStatus.OK)
 @RequestMapping("/locations")
-class LocationsController(val service : LocationsService) : Controller<LocationsWrapper>{
+class LocationsController(val service : LocationsService, val jwtService: JWTAuthenticator) : Controller<LocationsWrapper>{
 
     @GetMapping
     fun findAll(response: HttpServletResponse, request: HttpServletRequest): List<LocationsWrapper> {
@@ -21,7 +24,27 @@ class LocationsController(val service : LocationsService) : Controller<Locations
     @GetMapping(value = ["/{secret_id}"])
     fun findById(@PathVariable secret_id: UUID,
                  response: HttpServletResponse, request: HttpServletRequest): LocationsWrapper {
-        return service.findById(secret_id)
+        return service.findBySecretId(secret_id)
     }
 
+    @PostMapping
+    fun create(@RequestBody resource: LocationWrapper,
+               response: HttpServletResponse, request: HttpServletRequest): UUID{
+        resource.creatorId = Optional.of(jwtService.tryAuthenticate(request).id)
+        return service.create(resource)
+    }
+
+    @PatchMapping(value = ["/{secret_id}"])
+    fun update(@PathVariable secret_id: UUID, @RequestBody resource: LocationWrapper,
+               response: HttpServletResponse, request: HttpServletRequest) {
+        if(jwtService.tryAuthenticate(request).id != service.findBySecretId(secret_id).creatorId)
+            throw GenericException("The currently logged in user did not create this location and can therefor not edit it.")
+        service.update(secret_id, resource)
+    }
+
+    @DeleteMapping(value = ["/{secret_id}"])
+    fun delete(@PathVariable secret_id: UUID,
+               response: HttpServletResponse, request: HttpServletRequest) {
+        service.deleteById(jwtService.tryAuthenticate(request), secret_id)
+    }
 }
--- src/main/kotlin/be/ugent/webdevelopment/backend/geocode/controllers/UserController.kt
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/controllers/UserController.kt
@@ -1,11 +1,12 @@
 package be.ugent.webdevelopment.backend.geocode.controllers
 
+import be.ugent.webdevelopment.backend.geocode.controllers.wrappers.LocationWrapper
 import be.ugent.webdevelopment.backend.geocode.controllers.wrappers.UserWrapper
 import be.ugent.webdevelopment.backend.geocode.services.JWTAuthenticator
+import be.ugent.webdevelopment.backend.geocode.services.LocationsService
 import be.ugent.webdevelopment.backend.geocode.services.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import java.lang.IllegalArgumentException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletResponse
 @RestController
 @ResponseStatus(HttpStatus.OK)
 @RequestMapping("/user")
-class UserController(val service: UserService, val jwtService: JWTAuthenticator) : Controller<UserWrapper>{
+class UserController(val service: UserService, val jwtService: JWTAuthenticator, val locationsService: LocationsService) : Controller<UserWrapper>{
 
     @GetMapping
     fun findByLoggedIn(
@@ -21,7 +22,12 @@ class UserController(val service: UserService, val jwtService: JWTAuthenticator)
         return UserWrapper(jwtService.tryAuthenticate(request))
     }
 
-    @PutMapping
+    @GetMapping(value = ["/locations"])
+    fun getLocations(response: HttpServletResponse, request: HttpServletRequest): List<LocationWrapper>{
+        return locationsService.findAllByUser(jwtService.tryAuthenticate(request))
+    }
+
+    @PatchMapping
     fun update(@RequestBody resource: UserWrapper,
                response: HttpServletResponse, request: HttpServletRequest) {
         service.update(jwtService.tryAuthenticate(request), resource)
