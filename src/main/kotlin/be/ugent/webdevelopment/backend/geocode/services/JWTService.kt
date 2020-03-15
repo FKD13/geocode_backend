89044fdb6e6bb4b48d0946e35a394f179a36bc1a
Francis <francisklinck@gmail.com>
2020-03-15 21:12:48 +0100

    disable secure for now

--- src/main/kotlin/be/ugent/webdevelopment/backend/geocode/services/JWTService.kt
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/services/JWTService.kt
@@ -63,6 +63,7 @@ class JWTAuthenticator {
         response.addCookie(Cookie(cookieToken, JWT.create()
                 .withSubject(user.id.toString())
                 .withExpiresAt(Date(System.currentTimeMillis() + secretDuration.toLong()))
-                .sign(Algorithm.HMAC512(secret))).also {it.isHttpOnly = true}.also {it.secure = true}.also {it.path = "/"}) //TODO be more specific than "/"
+                .sign(Algorithm.HMAC512(secret))).also {it.isHttpOnly = true}.also {it.path = "/"}) //TODO: be more specific than "/"
+        //TODO: add secure
     }
 }
