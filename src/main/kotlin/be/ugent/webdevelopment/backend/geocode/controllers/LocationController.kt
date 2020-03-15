3d9562bc2a098994714a553a182fedd50ec77988
Arthur Deruytter <arthur.deruytter@ugent.be>
2020-03-15 05:32:27 +0100

    Whoops paar dingen vergeten

--- src/main/kotlin/be/ugent/webdevelopment/backend/geocode/controllers/LocationController.kt
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/controllers/LocationController.kt
@@ -1,6 +1,7 @@
 package be.ugent.webdevelopment.backend.geocode.controllers
 
 import be.ugent.webdevelopment.backend.geocode.controllers.wrappers.LocationWrapper
+import be.ugent.webdevelopment.backend.geocode.services.JWTAuthenticator
 import be.ugent.webdevelopment.backend.geocode.services.LocationService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -11,22 +12,24 @@ import javax.servlet.http.HttpServletResponse
 @RequestMapping("/Location")
 @ResponseStatus(HttpStatus.OK)
 @RestController
-class LocationController(val service: LocationService) : Controller<LocationWrapper>{
+class LocationController(val service: LocationService, val jwtService: JWTAuthenticator) : Controller<LocationWrapper>{
 
     @GetMapping
     fun findAll(response: HttpServletResponse, request: HttpServletRequest): List<LocationWrapper> {
-        return service.findAll(1)
+        return service.findAll(jwtService.tryAuthenticateGetId(request))
     }
 
     @PostMapping
     fun create(@RequestBody resource: LocationWrapper,
                response: HttpServletResponse, request: HttpServletRequest): UUID{
+        resource.creatorId = Optional.of(jwtService.tryAuthenticateGetId(request))
         return service.create(resource)
     }
 
     @PutMapping(value = ["/{secret_id}"])
     fun update(@PathVariable secret_id: UUID, @RequestBody resource: LocationWrapper,
                response: HttpServletResponse, request: HttpServletRequest) {
+        //todo double check of dat de location met die secret_id ook effectief van de ingelogde user is.
         service.update(secret_id, resource)
     }
 
