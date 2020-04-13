a25168e53e79b270aaac19654977140510db9772
fklinck <francis.klinck@ugent.be>
2020-04-13 16:22:58 +0200

    update QRCodeService.kt

--- src/main/kotlin/be/ugent/webdevelopment/backend/geocode/services/QRCodeService.kt
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/services/QRCodeService.kt
@@ -3,38 +3,22 @@ package be.ugent.webdevelopment.backend.geocode.services
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import com.google.zxing.qrcode.QRCodeWriter
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.awt.image.BufferedImage
 
 @Service
 class QRCodeService {
-    /*
-    * Then frontend url that should be uses in the url path
-    * */
-    @Value("\${GEOCODE_FRONTEND_URL}")
-    private lateinit var frontendUrl: String
 
-    private final val visitUrl = "locations/visits/"
-
-    /*
-    * The size in pixels of the qrcode
-    * */
-    @Value("\${GEOCODE_QRCODE_SIZE}")
-    private lateinit var qrcodeSize: String
-
-    /*
-    * Return a QRCode given the locations visit secret
-    * */
-    fun getQRCode(visitSecret: String) : BufferedImage {
+    /**
+     * Return a QRCode given the locations visit secret
+     * @param visitSecret the visit secret of the location
+     * @param frontendUrl the url of the frontend to add to the link
+     * @param size the size in pixels of the image
+     * @return a Bufferedimage representing the qrcode
+     */
+    fun getQRCode(visitSecret: String, frontendUrl: String, size: Int) : BufferedImage {
         // A little safe check
-        val url = if (frontendUrl.endsWith("/")) {
-            "${frontendUrl}${visitUrl}${visitSecret}"
-        } else {
-            "${frontendUrl}/${visitUrl}${visitSecret}"
-        }
-
-        val size = qrcodeSize.toIntOrNull() ?: throw IllegalArgumentException("GEOCODE_QRCODE_SIZE was not an Int")
+        val url = if (frontendUrl.endsWith("/")) frontendUrl + visitSecret else "$frontendUrl/$visitSecret"
 
         val qrCodeWriter = QRCodeWriter()
         val bitMatrix = qrCodeWriter.encode(url, BarcodeFormat.QR_CODE, size, size)
