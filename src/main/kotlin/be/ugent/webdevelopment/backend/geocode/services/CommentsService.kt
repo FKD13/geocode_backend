8bb51c55564ffa54418ba90cc569c3c83cf97422
Arthur Deruytter <arthur.deruytter@ugent.be>
2020-04-15 21:32:07 +0200

    smoll exception changes en name changes

--- src/main/kotlin/be/ugent/webdevelopment/backend/geocode/services/CommentsService.kt
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/services/CommentsService.kt
@@ -6,7 +6,6 @@ import be.ugent.webdevelopment.backend.geocode.database.models.User
 import be.ugent.webdevelopment.backend.geocode.database.repositories.CommentRepository
 import be.ugent.webdevelopment.backend.geocode.database.repositories.LocationRepository
 import be.ugent.webdevelopment.backend.geocode.exceptions.GenericException
-import be.ugent.webdevelopment.backend.geocode.exceptions.PropertyException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.time.Instant
@@ -21,54 +20,54 @@ class CommentsService {
     @Autowired
     lateinit var locationRepository: LocationRepository
 
-    fun getCommentsBySecretId(secret_id: UUID): List<Comment> {
-        val location = locationRepository.findBySecretId(secret_id = secret_id.toString())
+    fun getCommentsBySecretId(secretId: UUID): List<Comment> {
+        val location = locationRepository.findBySecretId(secretId = secretId.toString())
         if (location.isEmpty) {
             throw GenericException("Secret id is not linked to any location.")
         }
         return commentRepository.findAllByLocation(location = location.get())
     }
 
-    fun createComment(user: User, secret_id: UUID, comment: CommentWrapper) {
-        val location = locationRepository.findBySecretId(secret_id = secret_id.toString())
+    fun createComment(user: User, secretId: UUID, comment: CommentWrapper) {
+        val location = locationRepository.findBySecretId(secretId = secretId.toString())
         if (location.isEmpty) {
-            throw PropertyException("secret_id", "Secret id is not linked to any location.")
+            throw GenericException("Secret id is not linked to any location.")
         }
-        val commentString: String = comment.comment.orElseGet {""}
+        val commentString: String = comment.comment.orElseGet { "" }
         commentRepository.saveAndFlush(
                 Comment(creator = user, location = location.get(), createdAt = Date.from(Instant.now()), comment = commentString))
     }
 
     fun getCommentById(id: Int): Comment {
         val comment = commentRepository.findById(id)
-        if (comment.isEmpty){
-            throw PropertyException("commentId", "Comment with id = $id was not found.")
-        }else{
+        if (comment.isEmpty) {
+            throw GenericException("Comment with id = $id was not found.")
+        } else {
             return comment.get()
         }
     }
 
     fun updateComment(user: User, id: Int, comment: CommentWrapper) {
         commentRepository.findById(id).ifPresentOrElse({
-            if (it.creator.id != user.id){
+            if (it.creator.id != user.id) {
                 throw GenericException("The currently logged in user did not create this comment and can therefor not edit it.")
-            }else{
-                it.comment = comment.comment.orElseGet {""}
+            } else {
+                it.comment = comment.comment.orElseGet { "" }
             }
         }, {
-            throw PropertyException("commentId", "Comment with id = $id was not found.")
+            throw GenericException("Comment with id = $id was not found.")
         })
     }
 
     fun deleteCommentById(user: User, id: Int) {
         commentRepository.findById(id).ifPresentOrElse({
-            if (it.creator.id != user.id){
+            if (it.creator.id != user.id) {
                 throw GenericException("The currently logged in user did not create this comment and can therefor not delete it.")
-            }else{
+            } else {
                 commentRepository.delete(it)
             }
         }, {
-            throw PropertyException("commentId", "Comment with id = $id was not found.")
+            throw GenericException("Comment with id = $id was not found.")
         })
     }
 }
