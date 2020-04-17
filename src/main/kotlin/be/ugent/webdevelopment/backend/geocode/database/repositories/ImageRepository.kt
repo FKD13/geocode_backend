39a71a3e471da3a462965e0f310786ef36640b6a
Arthur Deruytter <arthur.deruytter@ugent.be>
2020-04-17 17:39:00 +0200

    gaat niet meer crashen op image upload

--- src/main/kotlin/be/ugent/webdevelopment/backend/geocode/database/repositories/ImageRepository.kt
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/database/repositories/ImageRepository.kt
@@ -3,6 +3,4 @@ package be.ugent.webdevelopment.backend.geocode.database.repositories
 import be.ugent.webdevelopment.backend.geocode.database.models.Image
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ImageRepository : JpaRepository<Image, Int> {
-
-}
+interface ImageRepository : JpaRepository<Image, Int>
