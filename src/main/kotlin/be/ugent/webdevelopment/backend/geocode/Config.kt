7116111935c2159975ca11e992aac7551671b39b
Arthur Deruytter <arthur.deruytter@ugent.be>
2020-05-01 21:02:47 +0200

    Apparte file voor Configuration

---
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/Config.kt
@@ -0,0 +1,9 @@
+package be.ugent.webdevelopment.backend.geocode
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+
+@Configuration
+@EnableAsync
+class Config
--- src/main/kotlin/be/ugent/webdevelopment/backend/geocode/utils/TraceConfig.kt
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/utils/TraceConfig.kt
@@ -2,8 +2,6 @@ package be.ugent.webdevelopment.backend.geocode.utils
 
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository
 import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.annotation.EnableAsync
 
 @Configuration
-@EnableAsync
 class TraceConfig : InMemoryHttpTraceRepository()
