acfa8f5c657ebd2cd35290c3ae8b81e4a8de033e
fklinck <francis.klinck@ugent.be>
2020-05-03 18:04:42 +0200

    add service to controller

--- src/main/kotlin/be/ugent/webdevelopment/backend/geocode/controllers/LeaderboardsController.kt
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/controllers/LeaderboardsController.kt
@@ -1,6 +1,7 @@
 package be.ugent.webdevelopment.backend.geocode.controllers
 
 import be.ugent.webdevelopment.backend.geocode.controllers.wrappers.LeaderboardsWrapper
+import be.ugent.webdevelopment.backend.geocode.services.LeaderboardsService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,30 +12,32 @@ import javax.websocket.server.PathParam
 @RestController
 @RequestMapping("/leaderboards")
 @ResponseStatus(HttpStatus.OK)
-class LeaderboardsController {
+class LeaderboardsController(
+        val leaderboardsService: LeaderboardsService
+) {
 
     @GetMapping("/visitedLocations")
     fun visitedLocations(@PathParam("limit") limit: Int): List<LeaderboardsWrapper> {
-        return emptyList()
+        return leaderboardsService.visitedLocations(limit)
     }
 
     @GetMapping("/visitedTours")
     fun visitedTours(@PathParam("limit") limit: Int): List<LeaderboardsWrapper> {
-        return emptyList()
+        return leaderboardsService.visitedTours(limit)
     }
 
     @GetMapping("/visitedCountries")
     fun visitedCountries(@PathParam("limit") limit: Int): List<LeaderboardsWrapper> {
-        return emptyList()
+        return leaderboardsService.visitedCountries(limit)
     }
 
     @GetMapping("/createdLocations")
     fun createdLocations(@PathParam("limit") limit: Int): List<LeaderboardsWrapper> {
-        return emptyList()
+        return leaderboardsService.createdLocations(limit)
     }
 
     @GetMapping("/createdTours")
     fun createdTours(@PathParam("limit") limit: Int): List<LeaderboardsWrapper> {
-        return emptyList()
+        return leaderboardsService.createdTours(limit)
     }
 }
