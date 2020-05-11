d20cd0630e352ec6926a55b846c650f291bbc732
maartenvn <maartenvn.eu@gmail.com>
2020-05-11 11:10:26 +0200

    Add current location endpoint

---
+++ src/main/kotlin/be/ugent/webdevelopment/backend/geocode/services/CurrentLocationService.kt
@@ -0,0 +1,29 @@
+package be.ugent.webdevelopment.backend.geocode.services
+
+import be.ugent.webdevelopment.backend.geocode.controllers.wrappers.CurrentLocationWrapper
+import be.ugent.webdevelopment.backend.geocode.exceptions.GenericException
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import java.net.URI
+
+@Service
+class CurrentLocationService {
+
+    /**
+     * Get the current location based on the received input IP.
+     * @param ip IP address to get the location for.
+     */
+    fun getCurrentLocation(ip: String): CurrentLocationWrapper {
+        // This is HTTP since HTTPS requires a payment plan.
+        val ipApiUrl = URI.create("http://ip-api.com/json/");
+
+        // Do the request
+        try {
+            return RestTemplate().getForObject(ipApiUrl, CurrentLocationWrapper::class.java)
+                    ?: throw GenericException("Unable to fetch current location.")
+        } catch (exception: Exception) {
+            throw GenericException("Unable to fetch current location.")
+        }
+    }
+}
